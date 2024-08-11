a34df90bb62538a07e347b7d8a3b803ba5d649b4
yaakoveidelman <yeidelman@eidelmans.com>
2024-08-11 09:53:02 -0400

    hw/2-21 fix

--- Database Files/Sprocs/RecipeDelete.sql
+++ Database Files/Sprocs/RecipeDelete.sql
@@ -3,21 +3,9 @@ as
 begin
     begin try
         begin tran
-            delete rd
-            from Recipe r
-            left join RecipeDirection rd 
-            on rd.RecipeId = r.RecipeId
-            where r.RecipeId = @recipeid
-
-            delete ri
-            from Recipe r 
-            left join RecipeIngredient ri 
-            on ri.RecipeId = r.RecipeId
-            where r.RecipeId = @recipeid
-
-            delete r
-            from Recipe r 
-            where r.RecipeId = @recipeid
+            delete RecipeDirection where RecipeId = @recipeid
+            delete RecipeIngredient where RecipeId = @recipeid
+            delete Recipe where RecipeId = @recipeid
         commit
     end try
     begin catch
