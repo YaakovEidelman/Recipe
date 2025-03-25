e217586a58e900fa925ee5ebf8b27fcacd596aaa
yaakoveidelman <yeidelman@eidelmans.com>
2025-03-25 19:27:07 -0400

    added cuisine class to bizSystem. added controler with endpoint for cuisine

---
+++ RecipeApps/RecipeAPI/Controllers/CuisineController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecipeSystem;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuisineController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<bizCuisine> c = new bizCuisine().GetList();
+            return Ok(c);
+        }
+    }
+}
---
+++ RecipeApps/RecipeSystem/bizCuisine.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeSystem
+{
+    public class bizCuisine : bizObject<bizCuisine>
+    {
+        public int CuisineId { get; set; }
+        public string CuisineType { get; set; } = "";
+    }
+}
